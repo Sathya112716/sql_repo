c9bed8d31438559ce9a743d183cdea618496898b
Sathya112716 <sathyapriya.r@diggibyte.com>
2024-03-18 16:48:35 +0530

    Create a new table as product_details and perform the query

--- src/sql_assignment2.sql
+++ src/sql_assignment2.sql
@@ -3,7 +3,7 @@ CREATE TABLE product_details (
     sell_date DATE,
     product VARCHAR(50)
 );
--- 2.Insert data into product_details
+-- 2.Insert data into product_details table
 INSERT INTO product_details (sell_date, product) VALUES
 ('2020-05-30', 'Headphones'),
 ('2020-06-01', 'Pencil'),
